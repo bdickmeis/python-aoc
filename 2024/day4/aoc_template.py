5de08a6f917e69581c59e10156b477c1b0af5752
bdickmei <bart.dickmeis@telenetgroup.be>
2024-12-04 15:56:15 +0100

    day 4 - start

---
+++ 2024/day4/aoc_template.py
@@ -0,0 +1,66 @@
+import pathlib
+import sys
+
+"""
+input:
+
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+
+hits:
+
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+"""
+
+word_search = "XMAS"
+
+  
+
+def parse(puzzle_input):
+    """Parse input"""
+    return [line for line in puzzle_input.split('\n')]
+
+
+def part1(data):
+    """Solve part 1"""
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            print(f'{data[row][col]}')
+
+
+def part2(data):
+    """Solve part 2"""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))

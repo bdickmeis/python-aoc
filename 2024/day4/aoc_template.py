00f4b4a081083c9061c172de6a731cc2303f94f7
bdickmei <bart.dickmeis@gmail.com>
2024-12-04 18:07:08 +0100

    day4 - initial idea

--- 2024/day4/aoc_template.py
+++ 2024/day4/aoc_template.py
@@ -29,20 +29,61 @@ S.S.S.S.SS
 .X.X.XMASX
 """
 
-word_search = "XMAS"
+word = "XMAS"
 
-  
+
+class Direction:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"index increment row+{self.x}, col+{self.y}"
+
+
+def is_valid_pos(row, col, data):
+    # check for boundaries
+    if 0 <= row <= len(data):
+        if 0 <= col <= len(data[row]):
+            return True
+
+    return False
+
+def check_next(letter,row,col,dir):
+    pass
 
 def parse(puzzle_input):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return [line for line in puzzle_input.split("\n")]
 
 
 def part1(data):
     """Solve part 1"""
-    for row in range(len(data)):
-        for col in range(len(data[row])):
-            print(f'{data[row][col]}')
+    directions = [
+        Direction(-1, -1),
+        Direction(-1, 0),
+        Direction(-1, 1),
+        Direction(0, -1),
+        Direction(0, 0),
+        Direction(0, 1),
+        Direction(1, -1),
+        Direction(1, 0),
+        Direction(1, 1),
+    ]
+
+    for row in range(0,len(data)):
+        for col in range(0,len(data[row])):
+            # check if first letter or last letter of the word is found
+            # string can be reversed as well
+            if data[row][col] == word[0]:
+                print(f'Starting letter found at {(row,col)}')
+                for dir in directions:
+                    if is_valid_pos(row+dir.x,col+dir.y,data):
+                        check_next(word[1],row+dir.x,col+dir.y,dir)
+            if data[row][col] == word[-1]:
+                print(f'Ending letter found at {(row,col)}')
+
+    return None
 
 
 def part2(data):
